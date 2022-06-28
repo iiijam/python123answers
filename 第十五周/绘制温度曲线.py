6c5d11a13b938514068dea193c06a38acca4e6d5
iiijam <francojia@outlook.com>
2022-06-28 09:37:28 +0800

    fix

--- 第十五周/绘制温度曲线.py
+++ 第十五周/绘制温度曲线.py
@@ -10,10 +10,10 @@ with open('9.1 某月温度.txt', 'r') as f:
         y.append(float(i.split()[1]))
         z.append(float(i.split()[2]))
 
-plt.ylim(-10,25)
+plt.ylim(-10, 25)
 
-plt.plot(x, y, linestyle='-', color='r', marker='o',)
-plt.plot(x, z, linestyle='-', color='b',marker= '*',)
+plt.plot(x, y, linestyle='-', color='r', marker='o')
+plt.plot(x, z, linestyle='-', color='b', marker='*')
 plt.axhline(y=0, color='b', linestyle='--')
 plt.xticks(np.arange(1, 32, 1))
 plt.savefig('temp_curve.png')
